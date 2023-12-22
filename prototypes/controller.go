77e90a411069e354540d6d9216a07256d6d87721
Cian Farrell <cianfarrell02@gmail.com>
2023-12-22 20:42:05 +0000

    Improved HUD and loading

---
+++ prototypes/controller.go
@@ -0,0 +1,69 @@
+components {
+  id: "controller"
+  component: "/scripts/controller.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "mainproxy"
+  type: "collectionproxy"
+  data: "collection: \"/main/game.collection\"\n"
+  "exclude: false\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "deathproxy"
+  type: "collectionproxy"
+  data: "collection: \"/main/death.collection\"\n"
+  "exclude: false\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "startproxy"
+  type: "collectionproxy"
+  data: "collection: \"/main/launchScreen.collection\"\n"
+  "exclude: false\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
