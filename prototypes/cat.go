96a3fbe3713c27c2432f6d4f9e0848fbe9f02354
Cian Farrell <cianfarrell02@gmail.com>
2023-12-22 22:33:43 +0000

    Added more sounds

--- prototypes/cat.go
+++ prototypes/cat.go
@@ -87,3 +87,72 @@ embedded_components {
     w: 1.0
   }
 }
+embedded_components {
+  id: "sound"
+  type: "sound"
+  data: "sound: \"/sounds/jump.wav\"\n"
+  "looping: 0\n"
+  "group: \"master\"\n"
+  "gain: 1.0\n"
+  "pan: 0.0\n"
+  "speed: 1.0\n"
+  "loopcount: 0\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "music"
+  type: "sound"
+  data: "sound: \"/sounds/music.wav\"\n"
+  "looping: 1\n"
+  "group: \"master\"\n"
+  "gain: 1.0\n"
+  "pan: 0.0\n"
+  "speed: 1.0\n"
+  "loopcount: 0\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "scream"
+  type: "sound"
+  data: "sound: \"/sounds/converted_cat_screaming.wav\"\n"
+  "looping: 0\n"
+  "group: \"master\"\n"
+  "gain: 1.0\n"
+  "pan: 0.0\n"
+  "speed: 1.0\n"
+  "loopcount: 0\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
